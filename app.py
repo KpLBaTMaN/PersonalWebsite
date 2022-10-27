c1a2879e72f2624457075f7eedcaa8842104f91f
Andrew Brown <kpl_batman123@live.co.uk>
2022-10-27 23:13:48 +0100

    update to system file system change added to requirements

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
-print("This is a test to see if merging works")
-print("Test 2")
+from flask import Flask, redirect, url_for, request, render_template
+from flask_assets import Environment, Bundle
+
+
+
+print("Loading App")
+APP = Flask(__name__,
+    static_url_path='',
+    static_folder="web/static/",
+    template_folder="web/templates/")
+
+
+@APP.route("/")
+def home():
+    return render_template('index.html')
+
+
+@APP.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+ 
+if __name__ == '__main__':
+    APP.debug=True
+    APP.run()
